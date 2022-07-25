10650eeaaaa1635d2f04531cd330c7ddfcd22a87
NevoTapiero <nevotap1234@gmail.com>
2022-07-25 18:04:56 +0300

    Update pythonReview.py

--- pythonReview.py
+++ pythonReview.py
@@ -1,6 +1,6 @@
 def create_youtube_video(title, description):
-	IshowSpeed = {"title": title, "description": description, "likes": 0, "dislikes": 0, "comments":{}}
-	return IshowSpeed
+	youtube_video_platform = {"title": title, "description": description, "likes": 0, "dislikes": 0, "comments":{}}
+	return youtube_video_platform
 
 title = input("What is your title? ")
 description = input("what is your youtube video description? ")
@@ -9,18 +9,25 @@ description = input("what is your youtube video description? ")
 new_youtube_video = create_youtube_video(title, description)
 print(new_youtube_video)
 
-def dislike(IshowSpeed):
-	IshowSpeed["likes"] += 1
-	return IshowSpeed
+def dislike(youtube_video):
+	youtube_video["dislikes"] += 1
+	return youtube_video
 
-def add_comment(IshowSpeed):
+def add_comment(youtube_video):
 	username = input("what is your name?")
 	comment_text = input("pls give a comment")
-	IshowSpeed["comments"][username] = comment_text
-	return IshowSpeed
+	youtube_video["comments"][username] = comment_text
+	return youtube_video
 
-def add_likes(IshowSpeed):
-	IshowSpeed["likes"] += 1
-	return IshowSpeed
+def add_likes(youtube_video):
+	youtube_video["likes"] += 1
+	return youtube_video
 
-print(IshowSpeed)
+dislike(new_youtube_video)
+
+for i in range(495):
+	add_likes(new_youtube_video)
+
+add_comment(new_youtube_video)
+
+print(new_youtube_video)
